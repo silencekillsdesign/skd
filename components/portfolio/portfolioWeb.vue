cdb9bf8d0d1587e05995e7a2ca0b49a32ab276e9
CJ Williams <cj@silencekillsdesign.com>
2019-08-24 12:27:57 -0600

    waaaayyy better component for portfolio under web

--- components/portfolio/portfolioWeb.vue
+++ components/portfolio/portfolioWeb.vue
@@ -1,35 +1,41 @@
 <template>
   <v-container fluid grid-list pa-0 ma-0>
     <v-layout row wrap ma-0>
-      <v-flex xs6 md4 ma-0 pa-0>
-        <v-card flat tile href="https://student.runneredq.com" target="_blank">
-          <v-img flat :aspect-ratio="9/6" :src=" require('@/assets/img/portfolio/web/web_clean-student.svg')" ></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 md4 ma-0 pa-0>
-        <v-card flat tile href="https://runneredq.com" target="_blank">
-          <v-img flat :aspect-ratio="9/6" :src=" require('@/assets/img/portfolio/web/web_runneredq.svg')" ></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 md4 ma-0 pa-0>
-        <v-card flat tile href="https://mudpaint.com" target="_blank">
-          <v-img flat :aspect-ratio="9/6" :src=" require('@/assets/img/portfolio/web/web_mudpaint.svg')" ></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 md4 ma-0 pa-0>
-        <v-card flat tile href="https://surveydig.com" target="_blank">
-          <v-img flat :aspect-ratio="9/6" :src=" require('@/assets/img/portfolio/web/web_surveydig.svg')" ></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 md4 ma-0 pa-0>
-        <v-card flat tile href="https://xtantmedical.com" target="_blank">
-          <v-img flat :aspect-ratio="9/6" :src=" require('@/assets/img/portfolio/web/web_xtant-medical.svg')" ></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 md4 ma-0 pa-0>
-        <v-card flat tile href="https://alumni.runneredq.com" target="_blank">
-          <v-img flat :aspect-ratio="9/6" :src=" require('@/assets/img/portfolio/web/web_clean-alumni.svg')" ></v-img>
-        </v-card>
+      <v-flex xs6 md4 ma-0 pa-0 v-for="(work,i) in works" :key="i">
+        <v-hover v-slot:default="{ hover }">
+          <v-card flat tile :href="work.link" target="_blank">
+            <v-img
+              flat
+              :aspect-ratio="9/6"
+              :alt="work.title"
+              :title="work.title"
+              :src=" require('@/assets/img/portfolio/web/'+work.image)"
+            >
+              <v-expand-transition>
+                <v-row
+                no-gutters
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out indigo accent-3 v-card--reveal headline white--text"
+                  style="height: 100%;"
+                >
+                  <v-col align="center" justify="center">
+                    <h2 class="mb-3">{{ work.title }}</h2>
+                    <v-btn
+                      outlined
+                      large
+                      color="white"
+                      type="a"
+                      :href="work.link"
+                      target="_blank"
+                      :title="work.title"
+                      :aria-labelledby="work.title"
+                    >View Website</v-btn>
+                  </v-col>
+                </v-row>
+              </v-expand-transition>
+            </v-img>
+          </v-card>
+        </v-hover>
       </v-flex>
     </v-layout>
   </v-container>
@@ -37,9 +43,52 @@
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      works: [
+        {
+          image: "web_clean-student.svg",
+          title: "CLEAN_Student Landing Page",
+          link: "https://student.runneredq.com"
+        },
+        {
+          image: "web_runneredq.svg",
+          title: "Runner EDQ Website",
+          link: "https:/runneredq.com"
+        },
+        {
+          image: "web_mudpaint.svg",
+          title: "MudPaint E-Commerce Site",
+          link: "https://mudpaint.com"
+        },
+        {
+          image: "web_surveydig.svg",
+          title: "SurveyDig Website",
+          link: "https://surveydig.com"
+        },
+        {
+          image: "web_xtant-medical.svg",
+          title: "Xtant Medical Website",
+          link: "https://xtantmedical.com"
+        },
+        {
+          image: "web_clean-alumni.svg",
+          title: "CLEAN_Alumni Landing Page",
+          link: "https://alumni.runneredq.com"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.9;
+  position: absolute;
+  width: 100%;
+}
 </style>
