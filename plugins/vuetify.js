7f141e760267ed3896f5e17846bb563be4da4a03
CJ Williams <cj@silencekillsdesign.com>
2019-09-06 11:15:46 -0600

    rm icons form vuetify plugin

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,15 +1,15 @@
 // src/plugins/vuetify.js
 
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+// import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
-  icons: {
-    iconfont: "mdi" // default - only for display purposes
-  },
+  // icons: {
+  //   iconfont: "mdi" // default - only for display purposes
+  // },
   theme: {
     options: {
       minifyTheme: function(css) {
